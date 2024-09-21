31704c86df275421649dfd4094beb79818450d78
jkhanjan <jhakhanjan62@gmail.com>
2024-09-21 23:50:48 +0530

    fifth

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -5,18 +5,26 @@ let isConnected = false; // Variable to track the connection status
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
 
-  if (!process.env.MONGODB_URI)
-    return console.log("MONGODB_URI is not defined");
+  if (!process.env.MONGODB_URI) {
+    console.error("MONGODB_URI is not defined");
+    throw new Error("MONGODB_URI is not defined");
+  }
 
-  if (isConnected) return console.log("=> using existing database connection");
+  if (isConnected) {
+    console.log("=> using existing database connection");
+    return;
+  }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      socketTimeoutMS: 30000, // Optional: Set a longer timeout
+    });
 
     isConnected = true;
 
     console.log("MongoDB Connected");
   } catch (error) {
-    console.log(error);
+    console.error("Error connecting to MongoDB:", error);
+    throw new Error("Failed to connect to MongoDB");
   }
 };
